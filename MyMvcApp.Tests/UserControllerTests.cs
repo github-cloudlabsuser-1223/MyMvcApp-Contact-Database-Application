e27908c57c5ade12907baed0d4bae1300d0d1101
azureuser <azureuser@example.com>
2025-06-26 10:28:31 +0000

    action fix

--- MyMvcApp.Tests/UserControllerTests.cs
+++ MyMvcApp.Tests/UserControllerTests.cs
@@ -1,10 +1,13 @@
-namespace MyMvcApp.Tests;
+using Xunit;
 
-public class UnitTest1
+namespace MyMvcApp.Tests
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        [Fact]
+        public void Test1()
+        {
 
+        }
     }
 }
